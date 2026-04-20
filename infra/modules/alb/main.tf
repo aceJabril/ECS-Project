2de57ba50ecb915a67ec1bd4a151a900f7af5770
Jabril Mahamed <acejabril@jabz-macbook.local>
2026-04-20 20:57:31 +0100

    fixing alb main.tf

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -18,6 +18,8 @@ resource "aws_lb_target_group" "ecs-tg" {
         port     = 80
         protocol = "HTTP"
         vpc_id   = var.vpc_id
+        target_type = "ip"
+
 
         health_check {
             path                = "/health"
