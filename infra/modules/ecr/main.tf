475af72b2f13516ac2a43c92121904043a608a7f
Jabril Mahamed <acejabril@jabz-macbook.local>
2026-04-22 15:01:38 +0100

    fixing ecr main.tf

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "ecs_project_ecr" {
     name = "ecs-project"
-    image_tag_mutability = "IMMUTABLE"
+    image_tag_mutability = "MUTTABLE"
     force_delete = true
 
     encryption_configuration {
